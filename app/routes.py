b89de886e09a231aaf78ddd9c2f3c624455c60f3
Roman Mytsko <roma.mytsko@icloud.com>
2019-12-11 19:48:44 +0200

    Added searching functional

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,13 @@ from app.forms import DataForm
 
 
 @app.route('/', methods=['GET', 'POST'])
-def search():
+def number():
     form = DataForm()
-    # if form.validate_on_submit():
-    #     var = "GOOD!"
-    #     return render_template('result.html', var=var)
+    if form.validate_on_submit():
+        my_list = {1: {'mytsko', 'baran', 'budzhak', 'motso'}, 2: {'smilka', 'soltys'}}
+        searching_last_name = form.data['last_name']
+        for table, peoples in my_list.items():
+            if searching_last_name in peoples:
+                our_table = table
+                return render_template('search.html', our_table=our_table)
     return render_template('main_page.html', form=form)
-
-
-
-
-
-
-
-# def index():
-#     return render_template('image.html')
